f010c6049fc88bf18c7a96c406ad5c29f8d29cac
Benedicte Raae <raae@raae.codes>
2021-04-09 20:20:42 +0200

    Initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <h1>404</h1>;
+};
+
+export default NotFoundPage;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IndexPage = () => {
+  return <h1>Nattermob.dev</h1>;
+};
+
+export default IndexPage;
