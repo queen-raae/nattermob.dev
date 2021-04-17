9baea08316b77d0ffb8a93f05b87d3e1497a8f9c
Benedicte Raae <raae@raae.codes>
2021-04-17 09:09:23 +0200

    Refactor from callback to await response.

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,29 +9,22 @@ exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
     auth: process.env.GOOGLE_API_KEY,
   });
 
-  await youtube.search.list(
-    {
-      channelId: "UCDlrzlRdM1vGr8nO708KFmQ",
-      part: "snippet",
-      maxResults: 2,
-      order: "date",
-      type: "video",
-    },
-    (error, response) => {
-      if (error) {
-        throw error;
-      }
+  const response = await youtube.search.list({
+    channelId: "UCDlrzlRdM1vGr8nO708KFmQ",
+    part: "snippet",
+    maxResults: 2,
+    order: "date",
+    type: "video",
+  });
 
-      response.data.items.forEach((video, index) => {
-        actions.createNode({
-          ...video,
-          id: createNodeId(`yt-${index}`),
-          internal: {
-            type: "yt",
-            contentDigest: createContentDigest(video),
-          },
-        });
-      });
-    }
-  );
+  response.data.items.forEach((video, index) => {
+    actions.createNode({
+      ...video,
+      id: createNodeId(`yt-${index}`),
+      internal: {
+        type: "yt",
+        contentDigest: createContentDigest(video),
+      },
+    });
+  });
 };
