e01a231160c864dd28fd9ac81bfbb579a8c3790c
Benedicte Raae <raae@raae.codes>
2021-04-21 06:56:58 +0200

    feat: Source YouTube videos for [issue #6, session 1, April 16th] (#11)
    * Hitting the youtube endpoint
    
    * Creating source nodes from the YT response
    
    * Refactor from callback to await response.
    
    * Better type name (as suggested by Paul on stream)

---
+++ gatsby-node.js
@@ -0,0 +1,30 @@
+const {google} = require("googleapis");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
+  const youtube = google.youtube({
+    version: "v3",
+    auth: process.env.GOOGLE_API_KEY,
+  });
+
+  const response = await youtube.search.list({
+    channelId: "UCDlrzlRdM1vGr8nO708KFmQ",
+    part: "snippet",
+    maxResults: 2,
+    order: "date",
+    type: "video",
+  });
+
+  response.data.items.forEach((video, index) => {
+    actions.createNode({
+      ...video,
+      id: createNodeId(`youtube-${index}`),
+      internal: {
+        type: "YouTube",
+        contentDigest: createContentDigest(video),
+      },
+    });
+  });
+};
