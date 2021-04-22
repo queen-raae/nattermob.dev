5eeb425ab972b7541df5bf1c3fcba009e5b9e1e4
pauliescanlon <pauliescanlon@gmail.com>
2021-04-22 10:48:09 +0100

    feat: change case on node type youTube

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,36 @@
-const {google} = require("googleapis");
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+const { google } = require('googleapis');
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
 });
 
-exports.sourceNodes = async ({actions, createNodeId, createContentDigest}) => {
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest
+}) => {
+  const YOUTUBE = 'youTube';
+
   const youtube = google.youtube({
-    version: "v3",
-    auth: process.env.GOOGLE_API_KEY,
+    version: 'v3',
+    auth: process.env.GOOGLE_API_KEY
   });
 
   const response = await youtube.search.list({
-    channelId: "UCDlrzlRdM1vGr8nO708KFmQ",
-    part: "snippet",
+    channelId: 'UCDlrzlRdM1vGr8nO708KFmQ',
+    part: 'snippet',
     maxResults: 2,
-    order: "date",
-    type: "video",
+    order: 'date',
+    type: 'video'
   });
 
   response.data.items.forEach((video, index) => {
     actions.createNode({
       ...video,
-      id: createNodeId(`youtube-${index}`),
+      id: createNodeId(`${YOUTUBE}-${index}`),
       internal: {
-        type: "YouTube",
-        contentDigest: createContentDigest(video),
-      },
+        type: YOUTUBE,
+        contentDigest: createContentDigest(video)
+      }
     });
   });
 };
