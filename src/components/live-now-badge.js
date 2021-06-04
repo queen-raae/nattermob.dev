a6a12b642a51d586d321c5360741530e5c70fc4b
Benedicte Raae <raae@raae.codes>
2021-06-04 21:08:29 +0200

    Even prettier live now badge

--- src/components/live-now-badge.js
+++ src/components/live-now-badge.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 
 const LiveNowBadge = () => {
   const [prefersReduceMotion, setPrefersReduceMotion] = useState(false)
+  const message = "LIVE NOW - youtube.com/raaecodes"
 
   useEffect(() => {
     if (window.matchMedia("(prefers-reduced-motion:reduce)").matches) {
@@ -10,8 +11,33 @@ const LiveNowBadge = () => {
   }, [])
 
   return (
-    <a href="http://youtube.com/raaecodes" target="_blank" rel="noreferrer">
-      {prefersReduceMotion ? <span>LIVE</span> : <marquee>LIVE</marquee>}
+    <a
+      style={{ textDecoration: "none" }}
+      href="http://youtube.com/raaecodes"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div
+        style={{
+          display: "grid",
+          alignItems: "center",
+          gridTemplateColumns: "auto 1fr",
+          backgroundColor: "#ff000090",
+          padding: "8px",
+          color: "white",
+        }}
+      >
+        <span aria-label="red circle" role="img">
+          🔴
+        </span>
+        <div style={{ background: "#ff0000" }}>
+          {prefersReduceMotion ? (
+            <span>{message}</span>
+          ) : (
+            <marquee style={{ paddingTop: "4px" }}>{message}</marquee>
+          )}
+        </div>
+      </div>
     </a>
   )
 }
