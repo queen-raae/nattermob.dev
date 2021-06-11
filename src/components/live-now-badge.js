66e7620a8d1e6294f9739d572bf52f5cffcecbfb
Benedicte Raae <raae@raae.codes>
2021-06-11 21:34:21 +0200

    Update badge

--- src/components/live-now-badge.js
+++ src/components/live-now-badge.js
@@ -1,12 +1,62 @@
 import React, { useState, useEffect } from "react"
 import { Fragment } from "react"
 
+const LiveNowContent = ({
+  prefersReducedMotion,
+  bgColor,
+  color = "#ffffff",
+  message,
+  ariaLabel,
+  emoji,
+}) => {
+  return (
+    <Fragment>
+      <span aria-label={ariaLabel} role="img">
+        {emoji}
+      </span>
+      <div style={{ color: color, background: bgColor }}>
+        {prefersReducedMotion ? (
+          <span style={{ paddingLeft: "4px" }}>{message}</span>
+        ) : (
+          <marquee style={{ paddingTop: "4px" }}>{message}</marquee>
+        )}
+      </div>
+    </Fragment>
+  )
+}
+
 const LiveNowBadge = () => {
-  const [isMounted, setIsMounted] = useState(false)
   const [prefersReducedMotion, setPrefersReducedMotion] = useState(false)
-  const message = "LIVE NOW - youtube.com/raaecodes"
+  const [isMounted, setIsMounted] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [areWeLive, setAreWeLive] = useState(false)
+  const [hasError, setHasError] = useState(false)
+
+  const liveMessage = "LIVE NOW - youtube.com/raaecodes"
+  const notLiveMessage = "NOT LIVE - youtube.com/raaecodes"
+  const errorMessage = "Blast! There's been an error"
+  const loadingMessage = "Loading..."
 
   useEffect(() => {
+    fetch("/api/are-we-live")
+      .then((response) => {
+        if (response.status >= 200 && response.status < 299) {
+          return response.json()
+        } else {
+          throw Error(response.message)
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        setAreWeLive(response.areWeLive)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        setHasError(true)
+        setIsLoading(false)
+      })
+
     setIsMounted(true)
   }, [])
 
@@ -20,6 +70,8 @@ const LiveNowBadge = () => {
       reduceQuery.removeEventListener("change", handleChange)
     }
   }, [isMounted])
+
+  console.log({ hasError, isLoading, areWeLive })
 
   return (
     <Fragment>
@@ -35,21 +87,53 @@ const LiveNowBadge = () => {
               display: "grid",
               alignItems: "center",
               gridTemplateColumns: "auto 1fr",
-              backgroundColor: "#ff000090",
               padding: "8px",
               color: "white",
             }}
           >
-            <span aria-label="red circle" role="img">
-              🔴
-            </span>
-            <div style={{ background: "#ff0000" }}>
-              {prefersReducedMotion ? (
-                <span style={{ paddingLeft: "4px" }}>{message}</span>
-              ) : (
-                <marquee style={{ paddingTop: "4px" }}>{message}</marquee>
-              )}
-            </div>
+            {isLoading ? (
+              <LiveNowContent
+                prefersReducedMotion={prefersReducedMotion}
+                color="#000000"
+                bgColor="#999999"
+                message={loadingMessage}
+                ariaLabel="anchor"
+                emoji="⚓"
+              />
+            ) : (
+              <Fragment>
+                {hasError ? (
+                  <LiveNowContent
+                    prefersReducedMotion={prefersReducedMotion}
+                    color="#000000"
+                    bgColor="#ffff00"
+                    message={errorMessage}
+                    ariaLabel="warning"
+                    emoji="⚠️"
+                  />
+                ) : (
+                  <Fragment>
+                    {areWeLive ? (
+                      <LiveNowContent
+                        prefersReducedMotion={prefersReducedMotion}
+                        bgColor="#ff0000"
+                        message={liveMessage}
+                        ariaLabel="red circle"
+                        emoji="🔴"
+                      />
+                    ) : (
+                      <LiveNowContent
+                        prefersReducedMotion={prefersReducedMotion}
+                        bgColor="#0000ff"
+                        message={notLiveMessage}
+                        ariaLabel={`three 0'clock`}
+                        emoji="🕒"
+                      />
+                    )}
+                  </Fragment>
+                )}
+              </Fragment>
+            )}
           </div>
         </a>
       ) : null}
