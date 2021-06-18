4248b6daef42c5ed756ea58c95a1b5d70005ef9e
Benedicte Raae <raae@raae.codes>
2021-06-18 13:56:45 +0200

    Inline style for longer description

--- src/pages/{youTube.slug}.js
+++ src/pages/{youTube.slug}.js
@@ -29,7 +29,7 @@ const YouTubePage = ({ data: { youTube } }) => {
             </a>
           </p>
         </aside>
-        <p>{description}</p>
+        <pre style={{ whiteSpace: "pre-wrap" }}>{description}</pre>
       </main>
     </Seo>
   )
