71d2ab7c4ce5372a690e2f5c9fcdd4397a6a49ed
pauliescanlon <pauliescanlon@gmail.com>
2021-08-02 08:41:03 +0100

    feat: add profile link to header

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react"
+import { Link } from "gatsby"
 import { useAuth0 } from "@auth0/auth0-react"
 
 const Profile = () => {
@@ -6,6 +7,7 @@ const Profile = () => {
 
   return (
     <main style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <Link to="/">← nattermob.dev</Link>
       {isAuthenticated ? (
         <Fragment>
           <h1>{`Ahoy ${user.nickname}`}</h1>
