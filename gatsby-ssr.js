7760235bf07beab3e3061197aa4d7d2f1a5b42f0
pauliescanlon <pauliescanlon@gmail.com>
2021-08-02 09:11:35 +0100

    feat: change gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,9 +4,9 @@ import RootElement from "./src/components/root-element"
 import Seo from "./src/components/seo"
 
 export const wrapPageElement = ({ element }) => {
-  return (
-    <RootElement>
-      <Seo>{element}</Seo>
-    </RootElement>
-  )
+  return <Seo>{element}</Seo>
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <RootElement>{element}</RootElement>
 }
