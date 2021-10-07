a29bf3817c56d2d46a357ab675364ad123267b19
Benedicte Raae <raae@raae.codes>
2021-10-07 14:16:37 +0200

    refactor: function cleanup (#47)
    * refactor: Add proper error handling and method validation
    
    * refactor: User proper methode -> GET
    
    * fix: Spelling error

--- src/api/are-we-live.js
+++ src/api/are-we-live.js
@@ -1,6 +1,7 @@
-const { google } = require("googleapis")
-const jwt = require("express-jwt")
-const jwks = require("jwks-rsa")
+import createError from "http-errors"
+import { google } from "googleapis"
+import jwt from "express-jwt"
+import jwks from "jwks-rsa"
 
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
@@ -27,46 +28,59 @@ const runJwtCheck = async (req, res) => {
 
 export default async function handler(req, res) {
   try {
-    // throw Error("Do not hit YOUTUBE")
-
     await runJwtCheck(req, res)
 
-    const youtube = google.youtube({
-      version: "v3",
-      auth: process.env.GOOGLE_API_KEY_SERVER,
-    })
-
-    const playlistItemsResponse = await youtube.playlistItems.list({
-      playlistId: "PL9W-8hhRoLoN7axEFJQ17rJvk2KTiM2GP",
-      part: "snippet",
-      maxResults: 50,
-      order: "date",
-      type: "video",
-    })
-
-    const ids = playlistItemsResponse.data.items.map(
-      (video) => video.snippet.resourceId.videoId
-    )
-
-    const videosResponse = await youtube.videos.list({
-      id: ids.join(","),
-      part: "snippet",
-    })
-
-    const areWeLive = videosResponse.data.items.some(
-      (video) => video.snippet.liveBroadcastContent === "live"
-    )
-
-    res.status(200).json({
-      messsage: areWeLive
-        ? "We are probably live 🎉"
-        : "We are probably not live 😥",
-      areWeLive,
-    })
+    if (req.method === "GET") {
+      // throw createError(418, `Do not hit the YouTube API`)
+      await getAreWeLive(req, res)
+    } else {
+      throw createError(405, `${req.method} not allowed`)
+    }
   } catch (error) {
-    res.status(error.status || 500).json({
-      message: error.inner?.message || error.message,
-      code: error.code,
+    const status = error.response?.status || error.statusCode || 500
+    const message = error.response?.data?.message || error.message
+
+    // Something went wrong, log it
+    console.error(`${status} -`, message)
+
+    // Respond with error code and message
+    res.status(status).json({
+      message: error.expose ? message : `Faulty ${req.baseUrl}`,
     })
   }
+}
+
+const getAreWeLive = async (req, res) => {
+  const youtube = google.youtube({
+    version: "v3",
+    auth: process.env.GOOGLE_API_KEY_SERVER,
+  })
+
+  const playlistItemsResponse = await youtube.playlistItems.list({
+    playlistId: "PL9W-8hhRoLoN7axEFJQ17rJvk2KTiM2GP",
+    part: "snippet",
+    maxResults: 50,
+    order: "date",
+    type: "video",
+  })
+
+  const ids = playlistItemsResponse.data.items.map(
+    (video) => video.snippet.resourceId.videoId
+  )
+
+  const videosResponse = await youtube.videos.list({
+    id: ids.join(","),
+    part: "snippet",
+  })
+
+  const areWeLive = videosResponse.data.items.some(
+    (video) => video.snippet.liveBroadcastContent === "live"
+  )
+
+  res.status(200).json({
+    message: areWeLive
+      ? "We are probably live 🎉"
+      : "We are probably not live 😥",
+    areWeLive: areWeLive,
+  })
 }
