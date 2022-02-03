a2538116cd62b02eee15b7dbccdef7a0ac231637
Ineza Bonté Grévy <inezabonte@gmail.com>
2022-02-03 08:16:05 -0500

    feat: add queen-red to links (#63)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              textDecorationColor: theme("colors.red.600"),
+              textDecorationThickness: "2px",
+            },
+            "a:hover": {
+              backgroundColor: theme("colors.red.600"),
+              color: theme("colors.white"),
+            },
+          },
+        },
+      }),
       gridTemplateColumns: {
         ["auto-1fr"]: "auto 1fr",
         ["1fr-auto"]: "1fr auto",
